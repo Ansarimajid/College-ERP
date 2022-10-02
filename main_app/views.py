40bfe9f9e8bcbea0a63041ff2904967cb4e0c42b
Ansari Majid Ali <86536394+Ansarimajid@users.noreply.github.com>
2022-10-02 12:38:27 +0530

    Update views.py

--- main_app/views.py
+++ main_app/views.py
@@ -30,7 +30,7 @@ def doLogin(request, **kwargs):
         #Google recaptcha
         captcha_token = request.POST.get('g-recaptcha-response')
         captcha_url = "https://www.google.com/recaptcha/api/siteverify"
-        captcha_key = "6LdWDQwiAAAAANRAEmOABUyxX8ZW-IMnIsFQ23bO"
+        captcha_key = "6LeaiEoiAAAAAFsKxkFYQbv_CDCSXegh-GEK1yYl"
         data = {
             'secret': captcha_key,
             'response': captcha_token
