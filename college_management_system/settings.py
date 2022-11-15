5227c1de0aa2f15ec4b67ae83ed421d5bd081e5a
Ansari Majid Ali <86536394+Ansarimajid@users.noreply.github.com>
2022-11-15 15:11:27 +0530

    Update settings.py

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -158,8 +158,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS') 
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('majidgt786@gmail.com') 
+EMAIL_HOST_PASSWORD = os.environ.get('esqmqqxxloseedtt')
 EMAIL_USE_TLS = True
 # DEFAULT_FROM_EMAIL = "School Management System <admin@admin.com>"
 
