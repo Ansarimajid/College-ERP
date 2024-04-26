0bb4ce0c0bdf914d647a8b573f23e648355b7373
K02D <kirondeb02@gmail.com>
2024-04-26 16:32:43 -0400

    revert

--- main_app/views.py
+++ main_app/views.py
@@ -7,27 +7,27 @@ from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.views.decorators.csrf import csrf_exempt
 
 from .EmailBackend import EmailBackend
-from .models import Attendance, Session, Subject
+from .models import Attendance, Session, Subject 
 
 # Create your views here.
 
 
 def login_page(request):
     if request.user.is_authenticated:
-        if request.user.user_type == "1":
+        if request.user.user_type == '1':
             return redirect(reverse("admin_home"))
-        elif request.user.user_type == "2":
+        elif request.user.user_type == '2':
             return redirect(reverse("staff_home"))
         else:
             return redirect(reverse("student_home"))
-    return render(request, "main_app/login.html")
+    return render(request, 'main_app/login.html')
 
 
 def doLogin(request, **kwargs):
-    if request.method != "POST":
+    if request.method != 'POST':
         return HttpResponse("<h4>Denied</h4>")
     else:
-        # Google recaptcha
+        #Google recaptcha
         # captcha_token = request.POST.get('g-recaptcha-response')
         # captcha_url = "https://www.google.com/recaptcha/api/siteverify"
         # captcha_key = "6LfHPwojAAAAAAtIjbi-7_N4fNf7Wp0LUiYlCDw_"
@@ -39,30 +39,27 @@ def doLogin(request, **kwargs):
         try:
             captcha_server = requests.post(url=captcha_url, data=data)
             response = json.loads(captcha_server.text)
-            if response["success"] == False:
-                messages.error(request, "Invalid Captcha. Try Again")
-                return redirect("/")
+            if response['success'] == False:
+                messages.error(request, 'Invalid Captcha. Try Again')
+                return redirect('/')
         except:
-            messages.error(request, "Captcha could not be verified. Try Again")
-            return redirect("/")
-
-        # Authenticate
-        user = EmailBackend.authenticate(
-            request,
-            username=request.POST.get("email"),
-            password=request.POST.get("password"),
-        )
+            messages.error(request, 'Captcha could not be verified. Try Again')
+            return redirect('/')
+        
+        #Authenticate
+        user = EmailBackend.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
         if user != None:
             login(request, user)
-            if user.user_type == "1":
+            if user.user_type == '1':
                 return redirect(reverse("admin_home"))
-            elif user.user_type == "2":
+            elif user.user_type == '2':
                 return redirect(reverse("staff_home"))
             else:
                 return redirect(reverse("student_home"))
         else:
             messages.error(request, "Invalid details")
             return redirect("/")
+
 
 
 def logout_user(request):
@@ -73,8 +70,8 @@ def logout_user(request):
 
 @csrf_exempt
 def get_attendance(request):
-    subject_id = request.POST.get("subject")
-    session_id = request.POST.get("session")
+    subject_id = request.POST.get('subject')
+    session_id = request.POST.get('session')
     try:
         subject = get_object_or_404(Subject, id=subject_id)
         session = get_object_or_404(Session, id=session_id)
@@ -82,10 +79,10 @@ def get_attendance(request):
         attendance_list = []
         for attd in attendance:
             data = {
-                "id": attd.id,
-                "attendance_date": str(attd.date),
-                "session": attd.session.id,
-            }
+                    "id": attd.id,
+                    "attendance_date": str(attd.date),
+                    "session": attd.session.id
+                    }
             attendance_list.append(data)
         return JsonResponse(json.dumps(attendance_list), safe=False)
     except Exception as e:
@@ -126,4 +123,5 @@ messaging.setBackgroundMessageHandler(function (payload) {
     return self.registration.showNotification(payload.notification.title, notificationOption);
 });
     """
-    return HttpResponse(data, content_type="application/javascript")
+    return HttpResponse(data, content_type='application/javascript')
+
