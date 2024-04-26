b627fc9f9a87bf183e801ebc0aa2fce66f1b78c6
K02D <kirondeb02@gmail.com>
2024-04-26 02:49:09 -0400

    add comment

--- main_app/staff_views.py
+++ main_app/staff_views.py
@@ -3,7 +3,7 @@ import json
 from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import (HttpResponseRedirect, get_object_or_404,redirect, render)
+from django.shortcuts import HttpResponseRedirect, get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 
@@ -11,6 +11,7 @@ from .forms import *
 from .models import *
 from . import forms, models
 from datetime import date
+
 
 def staff_home(request):
     staff = get_object_or_404(Staff, admin=request.user)
@@ -27,15 +28,22 @@ def staff_home(request):
         subject_list.append(subject.name)
         attendance_list.append(attendance_count)
     context = {
-        'page_title': 'Staff Panel - ' + str(staff.admin.first_name) + ' ' + str(staff.admin.last_name[0]) + '' + ' (' + str(staff.course) + ')',
-        'total_students': total_students,
-        'total_attendance': total_attendance,
-        'total_leave': total_leave,
-        'total_subject': total_subject,
-        'subject_list': subject_list,
-        'attendance_list': attendance_list
+        "page_title": "Staff Panel - "
+        + str(staff.admin.first_name)
+        + " "
+        + str(staff.admin.last_name[0])
+        + ""
+        + " ("
+        + str(staff.course)
+        + ")",
+        "total_students": total_students,
+        "total_attendance": total_attendance,
+        "total_leave": total_leave,
+        "total_subject": total_subject,
+        "subject_list": subject_list,
+        "attendance_list": attendance_list,
     }
-    return render(request, 'staff_template/home_content.html', context)
+    return render(request, "staff_template/home_content.html", context)
 
 
 def staff_take_attendance(request):
@@ -43,41 +51,42 @@ def staff_take_attendance(request):
     subjects = Subject.objects.filter(staff_id=staff)
     sessions = Session.objects.all()
     context = {
-        'subjects': subjects,
-        'sessions': sessions,
-        'page_title': 'Take Attendance'
+        "subjects": subjects,
+        "sessions": sessions,
+        "page_title": "Take Attendance",
     }
 
-    return render(request, 'staff_template/staff_take_attendance.html', context)
+    return render(request, "staff_template/staff_take_attendance.html", context)
 
 
 @csrf_exempt
 def get_students(request):
-    subject_id = request.POST.get('subject')
-    session_id = request.POST.get('session')
+    subject_id = request.POST.get("subject")
+    session_id = request.POST.get("session")
     try:
         subject = get_object_or_404(Subject, id=subject_id)
         session = get_object_or_404(Session, id=session_id)
-        students = Student.objects.filter(
-            course_id=subject.course.id, session=session)
+        students = Student.objects.filter(course_id=subject.course.id, session=session)
         student_data = []
         for student in students:
             data = {
-                    "id": student.id,
-                    "name": student.admin.last_name + " " + student.admin.first_name
-                    }
+                "id": student.id,
+                "name": student.admin.last_name + " " + student.admin.first_name,
+            }
             student_data.append(data)
-        return JsonResponse(json.dumps(student_data), content_type='application/json', safe=False)
+        return JsonResponse(
+            json.dumps(student_data), content_type="application/json", safe=False
+        )
     except Exception as e:
         return e
 
 
 @csrf_exempt
 def save_attendance(request):
-    student_data = request.POST.get('student_ids')
-    date = request.POST.get('date')
-    subject_id = request.POST.get('subject')
-    session_id = request.POST.get('session')
+    student_data = request.POST.get("student_ids")
+    date = request.POST.get("date")
+    subject_id = request.POST.get("subject")
+    session_id = request.POST.get("session")
     students = json.loads(student_data)
     try:
         session = get_object_or_404(Session, id=session_id)
@@ -86,8 +95,12 @@ def save_attendance(request):
         attendance.save()
 
         for student_dict in students:
-            student = get_object_or_404(Student, id=student_dict.get('id'))
-            attendance_report = AttendanceReport(student=student, attendance=attendance, status=student_dict.get('status'))
+            student = get_object_or_404(Student, id=student_dict.get("id"))
+            attendance_report = AttendanceReport(
+                student=student,
+                attendance=attendance,
+                status=student_dict.get("status"),
+            )
             attendance_report.save()
     except Exception as e:
         return None
@@ -95,49 +108,59 @@ def save_attendance(request):
     return HttpResponse("OK")
 
 
+# Note: testing "update_attendance" in staff_views.py isn't possible because in urls.py,
+# "staff_update_attendance" and "update_attendance" map to the same endpoint. So whenever
+# that endpoint is hit, "staff_update_attendance" gets executed.
 def staff_update_attendance(request):
     staff = get_object_or_404(Staff, admin=request.user)
     subjects = Subject.objects.filter(staff_id=staff)
     sessions = Session.objects.all()
     context = {
-        'subjects': subjects,
-        'sessions': sessions,
-        'page_title': 'Update Attendance'
+        "subjects": subjects,
+        "sessions": sessions,
+        "page_title": "Update Attendance",
     }
 
-    return render(request, 'staff_template/staff_update_attendance.html', context)
+    return render(request, "staff_template/staff_update_attendance.html", context)
 
 
 @csrf_exempt
 def get_student_attendance(request):
-    attendance_date_id = request.POST.get('attendance_date_id')
+    attendance_date_id = request.POST.get("attendance_date_id")
     try:
         date = get_object_or_404(Attendance, id=attendance_date_id)
         attendance_data = AttendanceReport.objects.filter(attendance=date)
         student_data = []
         for attendance in attendance_data:
-            data = {"id": attendance.student.admin.id,
-                    "name": attendance.student.admin.last_name + " " + attendance.student.admin.first_name,
-                    "status": attendance.status}
+            data = {
+                "id": attendance.student.admin.id,
+                "name": attendance.student.admin.last_name
+                + " "
+                + attendance.student.admin.first_name,
+                "status": attendance.status,
+            }
             student_data.append(data)
-        return JsonResponse(json.dumps(student_data), content_type='application/json', safe=False)
+        return JsonResponse(
+            json.dumps(student_data), content_type="application/json", safe=False
+        )
     except Exception as e:
         return e
 
 
 @csrf_exempt
 def update_attendance(request):
-    student_data = request.POST.get('student_ids')
-    date = request.POST.get('date')
+    student_data = request.POST.get("student_ids")
+    date = request.POST.get("date")
     students = json.loads(student_data)
     try:
         attendance = get_object_or_404(Attendance, id=date)
 
         for student_dict in students:
-            student = get_object_or_404(
-                Student, admin_id=student_dict.get('id'))
-            attendance_report = get_object_or_404(AttendanceReport, student=student, attendance=attendance)
-            attendance_report.status = student_dict.get('status')
+            student = get_object_or_404(Student, admin_id=student_dict.get("id"))
+            attendance_report = get_object_or_404(
+                AttendanceReport, student=student, attendance=attendance
+            )
+            attendance_report.status = student_dict.get("status")
             attendance_report.save()
     except Exception as e:
         return None
@@ -149,19 +172,20 @@ def staff_apply_leave(request):
     form = LeaveReportStaffForm(request.POST or None)
     staff = get_object_or_404(Staff, admin_id=request.user.id)
     context = {
-        'form': form,
-        'leave_history': LeaveReportStaff.objects.filter(staff=staff),
-        'page_title': 'Apply for Leave'
+        "form": form,
+        "leave_history": LeaveReportStaff.objects.filter(staff=staff),
+        "page_title": "Apply for Leave",
     }
-    if request.method == 'POST':
+    if request.method == "POST":
         if form.is_valid():
             try:
                 obj = form.save(commit=False)
                 obj.staff = staff
                 obj.save()
                 messages.success(
-                    request, "Application for leave has been submitted for review")
-                return redirect(reverse('staff_apply_leave'))
+                    request, "Application for leave has been submitted for review"
+                )
+                return redirect(reverse("staff_apply_leave"))
             except Exception:
                 messages.error(request, "Could not apply!")
         else:
@@ -173,18 +197,18 @@ def staff_feedback(request):
     form = FeedbackStaffForm(request.POST or None)
     staff = get_object_or_404(Staff, admin_id=request.user.id)
     context = {
-        'form': form,
-        'feedbacks': FeedbackStaff.objects.filter(staff=staff),
-        'page_title': 'Add Feedback'
+        "form": form,
+        "feedbacks": FeedbackStaff.objects.filter(staff=staff),
+        "page_title": "Add Feedback",
     }
-    if request.method == 'POST':
+    if request.method == "POST":
         if form.is_valid():
             try:
                 obj = form.save(commit=False)
                 obj.staff = staff
                 obj.save()
                 messages.success(request, "Feedback submitted for review")
-                return redirect(reverse('staff_feedback'))
+                return redirect(reverse("staff_feedback"))
             except Exception:
                 messages.error(request, "Could not Submit!")
         else:
@@ -194,17 +218,17 @@ def staff_feedback(request):
 
 def staff_view_profile(request):
     staff = get_object_or_404(Staff, admin=request.user)
-    form = StaffEditForm(request.POST or None, request.FILES or None,instance=staff)
-    context = {'form': form, 'page_title': 'View/Update Profile'}
-    if request.method == 'POST':
+    form = StaffEditForm(request.POST or None, request.FILES or None, instance=staff)
+    context = {"form": form, "page_title": "View/Update Profile"}
+    if request.method == "POST":
         try:
             if form.is_valid():
-                first_name = form.cleaned_data.get('first_name')
-                last_name = form.cleaned_data.get('last_name')
-                password = form.cleaned_data.get('password') or None
-                address = form.cleaned_data.get('address')
-                gender = form.cleaned_data.get('gender')
-                passport = request.FILES.get('profile_pic') or None
+                first_name = form.cleaned_data.get("first_name")
+                last_name = form.cleaned_data.get("last_name")
+                password = form.cleaned_data.get("password") or None
+                address = form.cleaned_data.get("address")
+                gender = form.cleaned_data.get("gender")
+                passport = request.FILES.get("profile_pic") or None
                 admin = staff.admin
                 if password != None:
                     admin.set_password(password)
@@ -220,13 +244,14 @@ def staff_view_profile(request):
                 admin.save()
                 staff.save()
                 messages.success(request, "Profile Updated!")
-                return redirect(reverse('staff_view_profile'))
+                return redirect(reverse("staff_view_profile"))
             else:
                 messages.error(request, "Invalid Data Provided")
-                return render(request, "staff_template/staff_view_profile.html", context)
+                return render(
+                    request, "staff_template/staff_view_profile.html", context
+                )
         except Exception as e:
-            messages.error(
-                request, "Error Occured While Updating Profile " + str(e))
+            messages.error(request, "Error Occured While Updating Profile " + str(e))
             return render(request, "staff_template/staff_view_profile.html", context)
 
     return render(request, "staff_template/staff_view_profile.html", context)
@@ -234,7 +259,7 @@ def staff_view_profile(request):
 
 @csrf_exempt
 def staff_fcmtoken(request):
-    token = request.POST.get('token')
+    token = request.POST.get("token")
     try:
         staff_user = get_object_or_404(CustomUser, id=request.user.id)
         staff_user.fcm_token = token
@@ -247,10 +272,7 @@ def staff_fcmtoken(request):
 def staff_view_notification(request):
     staff = get_object_or_404(Staff, admin=request.user)
     notifications = NotificationStaff.objects.filter(staff=staff)
-    context = {
-        'notifications': notifications,
-        'page_title': "View Notifications"
-    }
+    context = {"notifications": notifications, "page_title": "View Notifications"}
     return render(request, "staff_template/staff_view_notification.html", context)
 
 
@@ -259,27 +281,28 @@ def staff_add_result(request):
     subjects = Subject.objects.filter(staff=staff)
     sessions = Session.objects.all()
     context = {
-        'page_title': 'Result Upload',
-        'subjects': subjects,
-        'sessions': sessions
+        "page_title": "Result Upload",
+        "subjects": subjects,
+        "sessions": sessions,
     }
-    if request.method == 'POST':
+    if request.method == "POST":
         try:
-            student_id = request.POST.get('student_list')
-            subject_id = request.POST.get('subject')
-            test = request.POST.get('test')
-            exam = request.POST.get('exam')
+            student_id = request.POST.get("student_list")
+            subject_id = request.POST.get("subject")
+            test = request.POST.get("test")
+            exam = request.POST.get("exam")
             student = get_object_or_404(Student, id=student_id)
             subject = get_object_or_404(Subject, id=subject_id)
             try:
-                data = StudentResult.objects.get(
-                    student=student, subject=subject)
+                data = StudentResult.objects.get(student=student, subject=subject)
                 data.exam = exam
                 data.test = test
                 data.save()
                 messages.success(request, "Scores Updated")
             except:
-                result = StudentResult(student=student, subject=subject, test=test, exam=exam)
+                result = StudentResult(
+                    student=student, subject=subject, test=test, exam=exam
+                )
                 result.save()
                 messages.success(request, "Scores Saved")
         except Exception as e:
@@ -290,38 +313,36 @@ def staff_add_result(request):
 @csrf_exempt
 def fetch_student_result(request):
     try:
-        subject_id = request.POST.get('subject')
-        student_id = request.POST.get('student')
+        subject_id = request.POST.get("subject")
+        student_id = request.POST.get("student")
         student = get_object_or_404(Student, id=student_id)
         subject = get_object_or_404(Subject, id=subject_id)
         result = StudentResult.objects.get(student=student, subject=subject)
-        result_data = {
-            'exam': result.exam,
-            'test': result.test
-        }
+        result_data = {"exam": result.exam, "test": result.test}
         return HttpResponse(json.dumps(result_data))
     except Exception as e:
-        return HttpResponse('False')
+        return HttpResponse("False")
 
-#library
+
+# library
 def add_book(request):
     if request.method == "POST":
-        name = request.POST['name']
-        author = request.POST['author']
-        isbn = request.POST['isbn']
-        category = request.POST['category']
+        name = request.POST["name"]
+        author = request.POST["author"]
+        isbn = request.POST["isbn"]
+        category = request.POST["category"]
 
-
-        books = Book.objects.create(name=name, author=author, isbn=isbn, category=category )
+        books = Book.objects.create(
+            name=name, author=author, isbn=isbn, category=category
+        )
         books.save()
         alert = True
-        return render(request, "staff_template/add_book.html", {'alert':alert})
-    context = {
-        'page_title': "Add Book"
-    }
-    return render(request, "staff_template/add_book.html",context)
+        return render(request, "staff_template/add_book.html", {"alert": alert})
+    context = {"page_title": "Add Book"}
+    return render(request, "staff_template/add_book.html", context)
 
-#issue book
+
+# issue book
 
 
 def issue_book(request):
@@ -330,28 +351,41 @@ def issue_book(request):
         form = forms.IssueBookForm(request.POST)
         if form.is_valid():
             obj = models.IssuedBook()
-            obj.student_id = request.POST['name2']
-            obj.isbn = request.POST['isbn2']
+            obj.student_id = request.POST["name2"]
+            obj.isbn = request.POST["isbn2"]
             obj.save()
             alert = True
-            return render(request, "staff_template/issue_book.html", {'obj':obj, 'alert':alert})
-    return render(request, "staff_template/issue_book.html", {'form':form})
+            return render(
+                request, "staff_template/issue_book.html", {"obj": obj, "alert": alert}
+            )
+    return render(request, "staff_template/issue_book.html", {"form": form})
+
 
 def view_issued_book(request):
     issuedBooks = IssuedBook.objects.all()
     details = []
     for i in issuedBooks:
-        days = (date.today()-i.issued_date)
-        d=days.days
-        fine=0
-        if d>14:
-            day=d-14
-            fine=day*5
+        days = date.today() - i.issued_date
+        d = days.days
+        fine = 0
+        if d > 14:
+            day = d - 14
+            fine = day * 5
         books = list(models.Book.objects.filter(isbn=i.isbn))
         # students = list(models.Student.objects.filter(admin=i.admin))
-        i=0
+        i = 0
         for l in books:
-            t=(books[i].name,books[i].isbn,issuedBooks[0].issued_date,issuedBooks[0].expiry_date,fine)
-            i=i+1
+            t = (
+                books[i].name,
+                books[i].isbn,
+                issuedBooks[0].issued_date,
+                issuedBooks[0].expiry_date,
+                fine,
+            )
+            i = i + 1
             details.append(t)
-    return render(request, "staff_template/view_issued_book.html", {'issuedBooks':issuedBooks, 'details':details})
+    return render(
+        request,
+        "staff_template/view_issued_book.html",
+        {"issuedBooks": issuedBooks, "details": details},
+    )
