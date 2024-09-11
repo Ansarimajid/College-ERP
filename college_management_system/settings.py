e56d12af09b52894737ee92534617b56ae489db2
Ansari Majid Ali <taznoboys@gmail.com>
2024-09-11 07:22:30 +0000

    Fix Student Login Error

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'f2zx8*lb*em*-*b+!&1lpp&$_9q9kmkar+l3x90do@s(+sr&x7'  # Consider us
 DEBUG = True
 
 # ALLOWED_HOSTS = ['smswithdjango.herokuapp.com']
-ALLOWED_HOSTS = ['sigce-college-portal.up.railway.app','collegeportal.tk','sigce.ml']  # Not recommended but useful in dev mode
+ALLOWED_HOSTS = ['*']  # Not recommended but useful in dev mode
 
 
 # Application definition
